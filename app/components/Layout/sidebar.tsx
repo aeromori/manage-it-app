663a43767f35e73533b9472bd07fdace9bfbe43e
Adrian Cabrera Tolero <adrian@beautitag.com>
2025-11-17 15:55:18 +0800

    Added sidebar component and minor fix

---
+++ app/components/Layout/sidebar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+    HomeIcon,
+    Cog6ToothIcon,
+    UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+const routes: { name: string; path: string; icon?: any }[] = [
+    {
+        name: "Dashboard",
+        path: "/",
+        icon: <HomeIcon className="w-6 h-6" />,
+    },
+    {
+        name: "Tasks",
+        path: "/tasks",
+        icon: <Cog6ToothIcon className="w-6 h-6" />,
+    },
+    {
+        name: "Users",
+        path: "/users",
+        icon: <UserGroupIcon className="w-6 h-6" />,
+    },
+];
+
+const SideBar = () => {
+    const [activePage, setActivePage] = useState<string>("/");
+
+    return (
+        <>
+            <ul className="space-y-2 mt-4 text-emerald-700">
+                {routes.map((route) => (
+                    <li key={route.path}>
+                        <a
+                            href={route.path}
+                            className="flex items-center gap-2 p-2 rounded font-medium hover:bg-emerald-100"
+                        >
+                            <span className="flex items-center gap-2">
+                                {route.icon} {route.name}
+                            </span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default SideBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Layout/header";
+import SideBar from "./components/Layout/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,19 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Header />
+                {/* <Header /> */}
                 <div className="flex">
-                    <div className="w-1/8 h-screen pl-2 pr-2 bg-gray-100">
-                        Sidebar here
+                    <div className="w-1/6 h-screen pl-2 pr-2 bg-green-50">
+                        {/* Title */}
+                        <div className="p-2">
+                            <h4 className="text-4xl font-bold bg-linear-to-r from-green-500 to-emerald-600 bg-clip-text text-transparent tracking-tight">
+                                Manage It
+                            </h4>
+                        </div>
+                        {/* Sidebar and routes */}
+                        <div>
+                            <SideBar />
+                        </div>
                     </div>
                     <div className="flex-1 pl-2">{children}</div>
                 </div>
