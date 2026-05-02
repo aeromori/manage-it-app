ae393f0867fc57a1d7b8d8466b86adef1820e715
Adrian Cabrera Tolero <adrian@beautitag.com>
2026-05-02 20:37:46 +0800

    temporarily commented authprovider

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -1,82 +1,82 @@
-import React, {
-    createContext,
-    useContext,
-    useEffect,
-    useLayoutEffect,
-    useState,
-} from "react";
+// import React, {
+//     createContext,
+//     useContext,
+//     useEffect,
+//     useLayoutEffect,
+//     useState,
+// } from "react";
 
-import api from "../api/api";
+// import api from "../api/api";
 
-const AuthContext = createContext(undefined);
+// const AuthContext = createContext(undefined);
 
-export const useAuth = () => {
-    const authContext = useContext(AuthContext);
+// export const useAuth = () => {
+//     const authContext = useContext(AuthContext);
 
-    if (!authContext) {
-        throw new Error("useAuth must be used within a AuthProvider");
-    }
+//     if (!authContext) {
+//         throw new Error("useAuth must be used within a AuthProvider");
+//     }
 
-    return authContext;
-};
+//     return authContext;
+// };
 
-const AuthProvider = () => {
-    const [token, setToken] = useState<undefined | null | string>();
+// const AuthProvider = () => {
+//     const [token, setToken] = useState<undefined | null | string>();
 
-    useEffect(() => {
-        const fetchMe = async () => {
-            try {
-                const response = await api.get("/api/me");
-            } catch {
-                setToken(null);
-            }
-        };
+//     useEffect(() => {
+//         const fetchMe = async () => {
+//             try {
+//                 const response = await api.get("/api/me");
+//             } catch {
+//                 setToken(null);
+//             }
+//         };
 
-        fetchMe();
-    }, []);
+//         fetchMe();
+//     }, []);
 
-    useLayoutEffect(() => {
-        const authInterceptor = api.interceptors.request.use((config: any) => {
-            config.headers.authorization =
-                !config._retry && token
-                    ? `Bearer ${token}`
-                    : config.headers.authorization;
+//     useLayoutEffect(() => {
+//         const authInterceptor = api.interceptors.request.use((config: any) => {
+//             config.headers.authorization =
+//                 !config._retry && token
+//                     ? `Bearer ${token}`
+//                     : config.headers.authorization;
 
-            return config;
-        });
+//             return config;
+//         });
 
-        return () => {
-            api.interceptors.request.eject(authInterceptor);
-        };
-    }, [token]);
+//         return () => {
+//             api.interceptors.request.eject(authInterceptor);
+//         };
+//     }, [token]);
 
-    useLayoutEffect(() => {
-        const refreshInterceptor = api.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                const originalRequest = error.config;
+//     useLayoutEffect(() => {
+//         const refreshInterceptor = api.interceptors.response.use(
+//             (response) => response,
+//             async (error) => {
+//                 const originalRequest = error.config;
 
-                if (
-                    error.response.status === 403 &&
-                    error.response.data.message === "Unauthorized"
-                ) {
-                    try {
-                        const response = await api.get("/api/refreshToken");
+//                 if (
+//                     error.response.status === 403 &&
+//                     error.response.data.message === "Unauthorized"
+//                 ) {
+//                     try {
+//                         const response = await api.get("/api/refreshToken");
 
-                        setToken(response.data.accessToken);
+//                         setToken(response.data.accessToken);
 
-                        originalRequest.headers.authorization = `Bearer ${response.data.accessToken}`;
-                        originalRequest._retry = true;
+//                         originalRequest.headers.authorization = `Bearer ${response.data.accessToken}`;
+//                         originalRequest._retry = true;
 
-                        return api(originalRequest);
-                    } catch {
-                        setToken(null);
-                    }
-                }
-                return Promise.reject(error);
-            }
-        );
-    });
-};
+//                         return api(originalRequest);
+//                     } catch {
+//                         setToken(null);
+//                     }
+//                 }
+//                 return Promise.reject(error);
+//             }
+//         );
+//     });
+// };
 
-export default AuthProvider;
+// export default AuthProvider;
